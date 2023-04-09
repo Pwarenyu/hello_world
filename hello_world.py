305a43b5c8e485e6dd359d5e0d286f19954c9d98
Pwarenyu <Pwarenyu@gmail.com>
2023-04-09 13:15:15 +0800

    first commit

---
+++ hello_world.py
@@ -0,0 +1,31 @@
+message = "hello python world"
+print("hello python word!")
+
+message = "Hello Python Crash Course reader!"
+print(message)
+
+name ="ada lovelace"
+print(name.title())
+
+#f字符串
+first_name = "ada"
+last_name = "lovelace"
+full_name = f"{first_name} {last_name}"
+message = f"Hello, {full_name.title()}!"
+print(message)
+
+
+#制表符用\t,换行符用\n
+print("\thello\n\tpython")
+
+favorite_language = 'python '
+favorite_language.rstrip()
+
+
+message = "One of Python's strengths is its diverse community."
+print(message)
+
+
+MAX_CONNECTIONS =5000
+print(MAX_CONNECTIONS)
+
